c05561537b0894f6c4635dac9d58e376b2a187cd
smelendy <smelendy@umw.edu>
2019-11-11 10:08:13 -0500

    vminfo report

--- vminfo.ps1
+++ vminfo.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/pwsh
 
-Connect-viserver umw-vcenter
+Connect-viserver umw-vcenter.umw.local 
 
 Get-VM | Add-Member -Name "IPv4Address" -Value {$this.ExtensionData.Guest.IPAddress} -MemberType ScriptProperty -Passthru -Force | Add-Member -Name "GuestOS" -Value {$this.ExtensionData.Guest.GuestFullName} -MemberType ScriptProperty -Passthru -Force | Add-Member -Name "HostName" -Value {$this.ExtensionData.Guest.HostName} -MemberType ScriptProperty -Passthru -Force |Add-Member -Name "Datastore" -Value {Get-Datastore -VM $this} -MemberType ScriptProperty -Passthru -Force | Add-Member -Name "VMToolsVer" -Value {$this.extensiondata.config.tools.toolsversion} -MemberType ScriptProperty -Passthru -Force | Select IPv4Address,HostName,Name,VMHost,PowerState,VMToolsVer,GuestOS,Vendor,Model,Cpu,numCPU,MemoryGB,UsedSpaceGB,ProvisionedSpaceGB,Datastore,Notes | Export-excel -worksheetname Virutal-Guests  vmware.xlsx  
 
